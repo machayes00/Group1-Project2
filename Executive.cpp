66d12f8f50e0c4eee186f277ddec1535d7e2e966
n405m437 <90000136+n405m437@users.noreply.github.com>
2021-09-16 14:45:24 -0500

    Update Executive.cpp
    added some comments for the code

--- Executive.cpp
+++ Executive.cpp
@@ -25,20 +25,20 @@ void Executive::run()
   Player player2(ships);
   player1ships = ships;
   player2ships = ships;
-  int counter=0;
-  int row=0;
-  char column = ' ';
+  int counter=0; //counter to make sure all of the ships have been put onto the board.
+  int row=0; // the row in which the player want to add a ship.
+  char column = ' '; // the column in which the player wants to add a ship.
   do{
-    player1.getHiddenBoard().print();
+    player1.getHiddenBoard().print(); //returns the hidden board of the player class and prints it
     cout<<"where would you like to place a ship.\n";
     cout<<" Please enter a number for the row you would wish to put a ship in.\n";
     cin>>row;
     cout<<"Please enter a letter for the column you wish to put a ship in.\n";
     cin>>column;
-    player1.getHiddenBoard().exchange(row,column,'B');
-    player1.getHiddenBoard().print();
-    break;
-  } while(counter != player1ships);
+    player1.getHiddenBoard().exchange(row,column,'B'); // places the ship in the hidden board where the player has specified.
+    player1.getHiddenBoard().print(); // a check to make sure that the ship has been put in the correct spot
+    break;    //break for the loop since all the code hasnt been implemented
+  } while(counter != player1ships); //while loop to place the ships on the board, only for player1 at the moment.
 
 
   do {
