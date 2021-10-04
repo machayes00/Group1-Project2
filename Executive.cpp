897bbe0e7439d8a2cdc05e9481a7f1ac00963f0d
Edina Harsay <harsay@ku.edu>
2021-10-04 01:23:35 -0500

    Doxygen comments for Executive. Also, format fixes I fixed the formatting so that I can read the code easier for debugging. I pushed this commit so that the format fixes won't "burry" bug fixes and will be easier to see. I did not fix any bugs in this push (but I marked them in previous push).

--- Executive.cpp
+++ Executive.cpp
@@ -7,132 +7,143 @@
 #include <iostream>
 using namespace std;
 
-
 Executive::Executive()
 {
-
 }
 
 Executive::~Executive()
 {
-
 }
 
 void Executive::run()
 {
-  system("clear");
-  string tempinput;
-  string aitrigger;
+  system("clear");  // this clears the screen
+  string tempinput; // temporary variable for capturing user input for number of ships
+  string aitrigger; // variable for capturing user choice of AI or human player
   cout << "It is time for Batttle Ship!!!\n";
   do
   {
-    cout<<"Please enter a number between 1-6 of how many ships you would like to play with this game:\n";
-    cin>>tempinput;
+    cout << "Please enter a number between 1-6 for the number of ships you would like per player:\n";
+    cin >> tempinput;
 
-  }while(!(tempinput=="1" || tempinput=="2" || tempinput=="3" || tempinput=="4" || tempinput=="5" || tempinput=="6")); //runs loop until a valid number of ships is entered into the program
-  ships=stoi(tempinput);
-  cout<<"you have selected: "<<ships <<" ships to play with \n";
+  } while (!(tempinput == "1" || tempinput == "2" || tempinput == "3" || tempinput == "4" || 
+  tempinput == "5" || tempinput == "6")); //runs loop until a valid number of ships is entered into the program
+
+  ships = stoi(tempinput); // changes string input to int
+  cout << "You have selected: " << ships << " ships per player for this game. \n";
   player1ships = ships;
   player2ships = ships;
-  cout << "Do you want to play against an AI?(Y/N)";
+
+  cout << "Do you want to play against an AI instead of human opponent? (Y/N)";
   cin >> aitrigger;
-  cout << "Place the ships for player 1\n"; // place ships for palyer 1
+
+  cout << "Placing the ships for player 1\n"; // Player 1 ship placement same for AI opponent or human opponent
   place_ship(ships, player1);
-  system("clear");
-  if(aitrigger == "Y" || aitrigger == "y")
+
+  system("clear"); // screen clears after player 1 ships are placed, to prepare for player 2
+
+  if (aitrigger == "Y" || aitrigger == "y")
   {
-    cout << "Placing the ships for player 2\n"; // place ships for palyer 2
-    aiplace(ships,player2);
+    cout << "Placing the ships for player 2\n"; 
+    aiplace(ships, player2);
     system("clear");
   }
   else
   {
-  cout << "Place the ships for player 2\n"; // place ships for palyer 2
-  place_ship(ships, player2);
-  system("clear");
+    cout << "Place the ships for player 2\n"; 
+    place_ship(ships, player2);
+    system("clear");
   }
-  game_start(player1,player2,ships);
+
+  game_start(player1, player2, ships);
 
   return;
 }
 
-// E: please add comments to make it easier for me to help with this. I only marked syntax errors.
-// If comments clarify the intnet or plan, it makes it easier to fix the code.
-void Executive::aiplace(int size, Player& new_player)
+// Placing ships randomly for the AI opponent
+// E: I will explain the fixes in GitHub comments or Discord chat rather than here
+// E: Pushing a commit before I fix anything at all, because I fixed the formatting in this file
+// a lot (to help me read the code easier) and I do not want format changes to hide important changes
+void Executive::aiplace(int size, Player &new_player)
 {
   string tempinput; // E: tempinput is declared as both string and int
-  int num = 1; // reocord the size of ship
+  int num = 1;      // reocord the size of ship
   int row = 0;
   char column = ' ';
-  int tempinput;  // E: tempinput is declared as both string and int (I think 2nd declaration is ignored)
+  int tempinput; // E: tempinput is declared as both string and int (I think 2nd declaration is ignored)
   string dir;
 
-  do{
-      new_player.getGameBoard(); //returns the hidden board of the player class and prints it
-      srand(time(NULL));
-      do{
-      row=rand() % 9;
-      tempinput=rand() % 10;
+  do
+  {
+    new_player.getGameBoard(); //returns the hidden board of the player class and prints it
+    srand(time(NULL));
+    do
+    {
+      row = rand() % 9;
+      tempinput = rand() % 10;
       column = tempinput + 'A'; // E: column was declared as a char. Can't assign to a string OR int
-      tempinput=rand() % 2;
-      if(tempinput == 1) //  E: needs fix. C++ thinks you are comparing a string type to an int type
+      tempinput = rand() % 2;
+      if (tempinput == 1) //  E: needs fix. C++ thinks you are comparing a string type to an int type
       {
-        dir="row";
+        dir = "row";
       }
       else
       {
-        dir="col";
+        dir = "col";
       }
-      }while (!new_player.placeShip(row,column,num,dir));// places the ship in the hidden board where the player has specified.
+    } while (!new_player.placeShip(row, column, num, dir)); // places the ship in the hidden board where the player has specified.
 
-      cout << "Placed!\n";
-      //new_player.getGameBoard(); // a check to make sure that the ship has been put in the correct spot
-      num++;
-   } while(num < size+1);
+    cout << "Placed!\n";
+    //new_player.getGameBoard(); // a check to make sure that the ship has been put in the correct spot
+    num++;
+  } while (num < size + 1);
 
-   return;
+  return;
 }
-void Executive::place_ship(int size, Player& new_player)
+
+// Placing ship for human opponent
+void Executive::place_ship(int size, Player &new_player)
 {
   string tempinput;
-  int num = 1; // reocord the size of ship
+  int num = 1; // stores the size of ship
   int row = 0;
   char column = ' ';
   string direction;
-  do{
-      new_player.getGameBoard(); //returns the hidden board of the player class and prints it
+  do
+  {
+    new_player.getGameBoard(); //returns the hidden board of the player class and prints it
+    do
+    {
+
+      cout << "Where would you like to place a ship with size 1x" << num << "?\n";
+      cout << "The rest of the ship will extend down or right from your selected coordinate.\n";
+      cout << "If the placement is invalid your ship will not be placed and you will be prompted for new placemnet information.\n";
+
       do
       {
+        cout << "Please enter a number for the row you would wish to place a ship in.\n";
+        cin >> tempinput;
+      } while (!(tempinput == "1" || tempinput == "2" || tempinput == "3" || tempinput == "4" || tempinput == "5" || tempinput == "6" || tempinput == "7" || tempinput == "8" || tempinput == "9"));
+      row = stoi(tempinput);
+      cout << "Please enter a letter for the column you wish to place a ship in.\n";
+      cin >> column;
+      cout << "In which direction do you want to place? (row/col)";
+      cin >> direction;
+    } while (!new_player.placeShip(row, column, num, direction)); // places the ship in the hidden board where the player has specified.
 
-        cout<<"Where would you like to place a ship with size 1x" << num << "?\n";
-        cout<<"The rest of the ship will extend down or right from your selected coordinate.\n";
-        cout<<"If the placement is invalid your ship will not be placed and you will be prompted for new placemnet information.\n";
+    cout << "Placed!\n";
+    //new_player.getGameBoard(); // a check to make sure that the ship has been put in the correct spot
+    num++;
+  } while (num < size + 1);
 
-        do
-        {
-          cout<<"Please enter a number for the row you would wish to place a ship in.\n";
-          cin>>tempinput;
-        }while(!(tempinput=="1" || tempinput=="2" || tempinput=="3" || tempinput=="4" || tempinput=="5" || tempinput=="6" || tempinput=="7" || tempinput=="8" || tempinput=="9"));
-        row=stoi(tempinput);
-        cout<<"Please enter a letter for the column you wish to place a ship in.\n";
-        cin>>column;
-        cout << "In which direction do you want to place? (row/col)";
-        cin >> direction;
-      } while(!new_player.placeShip(row,column,num,direction));// places the ship in the hidden board where the player has specified.
-
-      cout << "Placed!\n";
-      //new_player.getGameBoard(); // a check to make sure that the ship has been put in the correct spot
-      num++;
-   } while(num < size+1);
-
-   return;
+  return;
 }
 
-void Executive::game_start(Player& player1, Player& player2, int size)
+void Executive::game_start(Player &player1, Player &player2, int size)
 {
   int player1_count = 0; //keeps a running total of the number of hits scored on each players side
   int player2_count = 0;
-  int max_count = size*(1+size)/2; //uses the given amount of ships(size) to check how many hits each player can have before they are out of ships.
+  int max_count = size * (1 + size) / 2; //uses the given amount of ships(size) to check how many hits each player can have before they are out of ships.
   char player_choice = 'n';
   int row = 0;
   char column = ' ';
@@ -144,123 +155,122 @@ void Executive::game_start(Player& player1, Player& player2, int size)
     cout << "Opponent's Board: choose a coordinate to fire at.\n";
     player2.getHiddenBoard();
 
-      do
-      {
-        cout << "Enter the the row number: ";  // get the row
-        cin>>tempinput;
+    do
+    {
+      cout << "Enter the the row number: "; // get the row
+      cin >> tempinput;
 
-      }while(!(tempinput=="1" || tempinput=="2" || tempinput=="3" || tempinput=="4" || tempinput=="5" || tempinput=="6" || tempinput=="7" || tempinput=="8" || tempinput=="9"));
-      row=stoi(tempinput);
-      do
-      {
-        cout << "Enter the column character: "; // get the column
-        cin >> column;
-        column_num = (char)column - 65;
-      }while (column_num<0 || column_num >10);//Boundary
+    } while (!(tempinput == "1" || tempinput == "2" || tempinput == "3" || tempinput == "4" || tempinput == "5" || tempinput == "6" || tempinput == "7" || tempinput == "8" || tempinput == "9"));
+    row = stoi(tempinput);
+    do
+    {
+      cout << "Enter the column character: "; // get the column
+      cin >> column;
+      column_num = (char)column - 65;
+    } while (column_num < 0 || column_num > 10); //Boundary
 
-
-      system("clear");
-      if (player2.checkHit(row, column))
+    system("clear");
+    if (player2.checkHit(row, column))
+    {
+      player2.get_hit(row, column);
+      player2_count++;
+      cout << "YOU HIT!\n";
+      player2.getHiddenBoard();
+      if (player2_count == max_count) //check if Player1 wins
       {
-        player2.get_hit(row,column);
-        player2_count++;
-        cout << "YOU HIT!\n";
-        player2.getHiddenBoard();
-        if (player2_count == max_count) //check if Player1 wins
-        {
-          break;
-        }
+        break;
       }
-      else
-      {
-        cout << "YOU MISSED!\n";
-        player2.getHiddenBoard();
-      }
+    }
+    else
+    {
+      cout << "YOU MISSED!\n";
+      player2.getHiddenBoard();
+    }
 
-      cout << "Do you want to see your Board(y/n)?\n";
+    cout << "Do you want to see your Board(y/n)?\n";
+    cin >> player_choice;
+    if (player_choice == 'y')
+    {
+      cout << "YOUR BOARD\n";
+      player1.getGameBoard();
+    }
+
+    do
+    {
+      cout << "End Turn(y/n)?\n";
       cin >> player_choice;
-      if(player_choice == 'y')
-      {
-        cout <<"YOUR BOARD\n";
-        player1.getGameBoard();
-      }
+    } while (player_choice != 'y');
+    system("clear");
 
+    do
+    {
+      cout << "Player2: Begin Turn(y/n)?\n";
+      cin >> player_choice;
+    } while (player_choice != 'y');
+    system("clear");
 
-      do
-      {
-        cout << "End Turn(y/n)?\n";
-        cin >> player_choice;
-      }while (player_choice != 'y');
-      system("clear");
+    cout << "======================PLAYER2=======================\n\n";
+    cout << "Opponent's Board: choose a coordinate to fire at.\n";
+    player1.getHiddenBoard();
 
-      do
-      {
-        cout << "Player2: Begin Turn(y/n)?\n";
-        cin >> player_choice;
-      }while (player_choice != 'y');
-      system("clear");
+    do
+    {
+      cout << "Enter the the row number: "; // get the row
+      cin >> tempinput;
 
-      cout << "======================PLAYER2=======================\n\n";
-      cout << "Opponent's Board: choose a coordinate to fire at.\n";
+    } while (!(tempinput == "1" || tempinput == "2" || tempinput == "3" || tempinput == "4" || tempinput == "5" || tempinput == "6" || tempinput == "7" || tempinput == "8" || tempinput == "9"));
+    row = stoi(tempinput);
+    do
+    {
+      cout << "Enter the column character: "; // get the column
+      cin >> column;
+      column_num = (char)column - 65;
+    } while (column_num < 0 || column_num > 10); //Boundary
+
+    system("clear");
+    if (player1.checkHit(row, column))
+    {
+      player1.get_hit(row, column);
+      player1_count++;
+      cout << "YOU HIT!\n";
       player1.getHiddenBoard();
-
-
-      do
+      if (player1_count == max_count) //check if Player2 wins
       {
-        cout << "Enter the the row number: ";  // get the row
-        cin>>tempinput;
-
-      }while(!(tempinput=="1" || tempinput=="2" || tempinput=="3" || tempinput=="4" || tempinput=="5" || tempinput=="6" || tempinput=="7" || tempinput=="8" || tempinput=="9"));
-      row=stoi(tempinput);
-      do
-      {
-        cout << "Enter the column character: "; // get the column
-        cin >> column;
-        column_num = (char)column - 65;
-      }while (column_num<0 || column_num >10); //Boundary
-
-      system("clear");
-      if (player1.checkHit(row, column))
-      {
-        player1.get_hit(row,column);
-        player1_count++;
-        cout << "YOU HIT!\n";
-        player1.getHiddenBoard();
-        if (player1_count == max_count) //check if Player2 wins
-        {
-          break;
-        }
+        break;
       }
-      else
-      {
-        cout << "YOU MISS!\n";
-        player1.getHiddenBoard();
-      }
+    }
+    else
+    {
+      cout << "YOU MISS!\n";
+      player1.getHiddenBoard();
+    }
 
-      cout << "Do you want to see your Board(y/n)?\n";
+    cout << "Do you want to see your Board(y/n)?\n";
+    cin >> player_choice;
+    if (player_choice == 'y')
+    {
+      cout << "YOUR BOARD\n";
+      player2.getGameBoard();
+    }
+    else
+    {
+    }
+
+    do
+    {
+      cout << "End Turn(y/n)?\n";
       cin >> player_choice;
-      if(player_choice == 'y')
-      {
-        cout <<"YOUR BOARD\n";
-        player2.getGameBoard();
-      }
-      else{}
+    } while (player_choice != 'y');
+    system("clear");
 
-      do
-      {
-        cout << "End Turn(y/n)?\n";
-        cin >> player_choice;
-      }while (player_choice != 'y');
-      system("clear");
+    do
+    {
+      cout << "Player1: Begin Turn(y/n)?\n";
+      cin >> player_choice;
+    } while (player_choice != 'y');
+    system("clear");
 
-      do
-      {
-        cout << "Player1: Begin Turn(y/n)?\n";
-        cin >> player_choice;
-      }while (player_choice != 'y');
-      system("clear");
-
-  }while (player1_count < max_count && player2_count < max_count);
+  } while (player1_count < max_count && player2_count < max_count);
   if (player1_count == max_count)
   {
     cout << "Player2 WINS!\n";
--- Executive.h
+++ Executive.h
@@ -12,35 +12,56 @@ private:
   int player1ships;
   int player2ships;
   int ships;
+
 public:
+  /**
+   * @brief Construct a new Executive object
+   * @pre none
+   * @return none
+   * @post a new Executive class object is created
+   */
   Executive();
 
+  /**
+   * @brief Destroy the Executive object
+   * @pre none
+   * @param none
+   * @post an executive class object is deallocated from memory
+   */
   ~Executive();
 
+  /**
+   * @brief Sets up the game and calls the other methods in the program and coordinates their actions
+   * @pre none
+   * @param none
+   * @return none
+   */
   void run();
-  //@input - none.
-  //@input - none.
-  //@process - This creates the space where the game is is run and dishes out tasks to the other methods ie the setup and game_start method.
-  //@return - none
 
+  /**
+   * @brief Places a ship with the passed-in size on the passed-in player's gameBoard.
+   * @param size the size of the ship being placed
+   * @param new_player Player object passed in by reference
+   * @return none
+   */
   void place_ship(int size, Player& new_player);
-  //@input - size - the size of the ship you wish to place at that coordinate.ie a 1x1 or 1x2 etc.
-  //@input - new_player - passes a player object by reference.
-  //@process - places a ship with the passed size on the passed players gameBoard.
-  //@return - none
-  
-  void aiplace(int size, Player& new_player);
-  //@input - size - the size of the ship you wish to place at that coordinate.ie a 1x1 or 1x2 etc.
-  //@input - new_player - passes a player object by reference.
-  //@process - AI randomly places ships on gameBoard.
-  //@return - none
 
+  /**
+   * @brief AI randomly places ships on gameBoard.
+   * @param size the size of the ship being placed
+   * @param new_player Player object passed in by reference
+   * @return none
+   */
+  void aiplace(int size, Player& new_player);
+
+  /**
+   * @brief Manages player turn-taking and checks for winner
+   * @param player1 Player object
+   * @param player2 Player object
+   * @param size The number of ships, 1-6
+   * return none
+   */
   void game_start(Player& player1, Player& player2, int size);
-  //@input - player1 - a passed player object.
-  //@input - player2 - a passed player object
-  //@input - size - amount of ships that game is currrently being player with between 1-6.
-  //@process - runs the games turns and checks for a win for both players.
-  //@return - none
 
 };
 
